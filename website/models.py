0b452e0fd88ac392e41197612dc8a2d9a4b756f0
Sezgin <adassezgin@gmail.com>
2023-04-10 17:27:19 +0300

    Place Table added

--- website/models.py
+++ website/models.py
@@ -1,6 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Place(models.Model):
+    place_name = models.CharField(max_length=50)
+    place_city = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.place_name
+    
 
 class Record(models.Model):
     created_at = models.DateTimeField(auto_now_add= True)
@@ -12,7 +19,11 @@ class Record(models.Model):
     city = models.CharField(max_length=50)
     state = models.CharField(max_length=50)
     zipcode = models.CharField(max_length=20)
+    person_place = models.ForeignKey(Place,blank=True,null=True,on_delete=models.CASCADE)
 
     def __str__(self):
         return (f"{self.first_name} {self.last_name}")
+    
+
+
     
